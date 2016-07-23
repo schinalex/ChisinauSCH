4d7b0a87914b8ec19320035e95b305610007c7c6
Alex <schinalex@gmail.com>
2016-07-23 12:48:24 +0300

    added mongoose

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
-var express = require('express')
-var app = express()
+(() => {
+    "use strict";
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+    const express = require('express'),
+        app = express(),
+        config = require('./modules/config'),
+        bodyParser = require('body-parser'),
+        assert = require('assert'),
+        mongoose = require('mongoose'),
+        usersRoutes = require('./routes/users');
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+    app.use(bodyParser.urlencoded({
+        extended: false
+    }));
+
+    app.use(bodyParser.json());
+    app.use(express.static('public'));
+    app.use(express.static('node_modules'));
+
+    mongoose.connect(config.database, (err) => {
+        assert.equal(null, err);
+        console.log('Successfuly connected to ' + config.database);
+    });
+
+    app.use('/api/v1/users', usersRoutes);
+
+    app.listen(config.port, () => {
+        console.log("Application is running on http://localhost:" + config.port + "/");
+    });
+})();
